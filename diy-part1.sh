c52c44f42f6cadf57d38e74fd9b0830ce3ba1e2a
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2022-11-22 13:32:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,11 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+#添加独立插件
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
-
+git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
