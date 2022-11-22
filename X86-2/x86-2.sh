4e40fc4173ec0ca524cd7d29ad2d6cd7f45c2cd7
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2022-11-23 02:16:48 +0800

    Update x86-2.sh

--- X86-2/x86-2.sh
+++ X86-2/x86-2.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git kiddin https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
