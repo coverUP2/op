276cf071568fdaf39a420793b6db7f4c94af1c56
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2022-11-24 01:49:27 +0800

    Update diy1.sh

--- lede/x86/diy1.sh
+++ lede/x86/diy1.sh
@@ -12,11 +12,12 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#neobird添加主题
+
+# 添加主题 neobird 
 git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
 
 
-#添加软件源
+# 添加软件源
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
