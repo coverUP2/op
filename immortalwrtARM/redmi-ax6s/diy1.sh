e95edd62b43d8dd7c4478094324548cc957b1b03
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2022-11-24 03:52:55 +0800

    Add files via upload

---
+++ immortalwrtARM/redmi-ax6s/diy1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 添加主题 neobird
+git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+
+# 添加软件源
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
