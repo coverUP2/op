bec37d6362c22a9b006c04aeeda91bca3c8e3d3a
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2022-11-24 01:49:02 +0800

    Update diy2.sh

--- lede/x86/diy2.sh
+++ lede/x86/diy2.sh
@@ -10,6 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 修改默认IP
 sed -i ' s/192.168.1.1/192.168.2.9/g ' package/base-files/files/bin/config_generate
 
