bbceed684535c3d24f591b56668f0bad319061fd
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-05-05 18:30:19 +0800

    Update diy1.sh

--- 5.15/x86/diy1.sh
+++ 5.15/x86/diy1.sh
@@ -19,5 +19,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sed -i '$a  src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
