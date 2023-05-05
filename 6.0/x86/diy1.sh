59ffd6ea959009202e06e29658a26568c83423a7
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-05-05 13:31:46 +0800

    Update diy1.sh

--- 6.0/x86/diy1.sh
+++ 6.0/x86/diy1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 
 # Add a feed source
