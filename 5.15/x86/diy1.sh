b5dc0418bfe8f874353bf4c2122d61fe42fe9ad1
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-06-03 04:20:27 +0800

    Update diy1.sh

--- 5.15/x86/diy1.sh
+++ 5.15/x86/diy1.sh
@@ -23,5 +23,4 @@
 #sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
