17330baecd9d60245192a625bc9eee5eda77a15b
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-06-03 09:38:47 +0800

    Update diy2.sh

--- 6.0/x86/diy2.sh
+++ 6.0/x86/diy2.sh
@@ -13,7 +13,6 @@
 # Modify default IP
 sed -i ' s/192.168.1.1/192.168.2.9/g ' package/base-files/files/bin/config_generate
 
-#修改内核版本
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
-
-sed -i ' s/luci-theme-bootstrap/luci-theme-argon/g ' feeds/luci/collections/luci/Makefile
+# 修改内核版本
+# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
+# sed -i ' s/luci-theme-bootstrap/luci-theme-argon/g ' feeds/luci/collections/luci/Makefile
