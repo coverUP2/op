c5688727155301cde2de5806ae8a5c63efdccffd
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-06-04 18:53:39 +0800

    Update diy2.sh

--- x86/istore/diy2.sh
+++ x86/istore/diy2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.2.9/g' package/base-files/files/bin/config_generate
 
 #修改内核版本
-sed -i ' s/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g ' target/linux/x86/Makefile
+# sed -i ' s/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g ' target/linux/x86/Makefile
 # 删除源
 # sed -i '/kenzok/d' /etc/opkg/distfeeds.conf
 # sed -i '/small/d' /etc/opkg/distfeeds.conf
