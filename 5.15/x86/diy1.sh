66b9f6749dfa50a486e68e235cd9aae249211399
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-06-13 09:35:26 +0800

    Update diy1.sh

--- 5.15/x86/diy1.sh
+++ 5.15/x86/diy1.sh
@@ -18,6 +18,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+
 
 #sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
