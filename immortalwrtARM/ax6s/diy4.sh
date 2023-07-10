6c3ccee91e63b0af51a4048cfea2ec55cfaec0ea
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-07-11 07:36:42 +0800

    Update diy4.sh

--- immortalwrtARM/ax6s/diy4.sh
+++ immortalwrtARM/ax6s/diy4.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改默认IP
-sed -i ' s/192.168.6.1/192.168.2.3/g ' package/base-files/files/bin/config_generate
+sed -i ' s/192.168.1.1/192.168.2.3/g ' package/base-files/files/bin/config_generate
 
 #sed -i ' s/luci-theme-bootstrap/luci-theme-argon-mod/g ' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
