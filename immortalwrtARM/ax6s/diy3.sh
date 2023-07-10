eb2a93ecc663a29de7d6e868593b93ab99b13067
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-07-11 00:13:39 +0800

    Update diy3.sh

--- immortalwrtARM/ax6s/diy3.sh
+++ immortalwrtARM/ax6s/diy3.sh
@@ -24,6 +24,3 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
