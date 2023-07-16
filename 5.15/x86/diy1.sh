82a726dd1642a9168bf335166f17546825d24ecb
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-07-17 06:11:50 +0800

    Update diy1.sh

--- 5.15/x86/diy1.sh
+++ 5.15/x86/diy1.sh
@@ -19,7 +19,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-rm -rf package/feeds/luci/luci-theme-argon
 git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon.git
 
 
