993328073430bc5ec1ebec2061c80a1750e64eb1
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-07-17 18:57:14 +0800

    Update diy1.sh

--- immortalwrtARM/ax6s/diy1.sh
+++ immortalwrtARM/ax6s/diy1.sh
@@ -15,7 +15,7 @@
 
 #git clone https://github.com/linkease/istore.git package/istore
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon.git
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon.git
 
 
 
