9369738b2e5dbdcd39a859e730eb54ec42400a78
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-07-17 20:47:59 +0800

    Update diy1.sh

--- x86/istore/diy1.sh
+++ x86/istore/diy1.sh
@@ -13,8 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-rm -rf /feeds/kenzo/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon2
+#rm -rf /feeds/kenzo/luci-theme-argon
+#git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon2
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 
 # Add a feed source
