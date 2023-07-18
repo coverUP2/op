a6f4f0fa5e124751357e8157fe099a0b660241af
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-07-19 01:48:47 +0800

    Update diy1.sh

--- 5.15/x86/diy1.sh
+++ 5.15/x86/diy1.sh
@@ -25,7 +25,8 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird 
 
 
-
+sed -i 's/src-git lienol https://github.com/Lienol/openwrt-package.git;main/# src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages;master' feeds.conf.default
+
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
