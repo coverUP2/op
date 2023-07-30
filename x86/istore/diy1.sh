20f320d4cc2f232c1718e180e01d40f1b3a0fdf0
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-07-30 18:55:46 +0800

    Update diy1.sh

--- x86/istore/diy1.sh
+++ x86/istore/diy1.sh
@@ -20,6 +20,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird 
 
 sed -i '$a src-git coverup https://github.com/coverUP2/openwrt-packages.git;master' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
