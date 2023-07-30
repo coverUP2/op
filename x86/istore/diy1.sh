3415b0ecd6a39b02742036d389631853a8b8f057
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-07-30 18:55:24 +0800

    Update diy1.sh

--- x86/istore/diy1.sh
+++ x86/istore/diy1.sh
@@ -21,7 +21,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
-sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+sed -i '$a src-git coverup https://github.com/coverUP2/openwrt-packages.git;master' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
