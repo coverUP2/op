85c39693642eebb06a6f9c77da015853cd64f625
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-08-29 10:55:37 +0800

    Delete script.sh

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd openwrt
-
-sed -i 's/192.168.1.1/192.168.2.9/g' package/base-files/files/bin/config_generate
-sed -i ' s/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g ' target/linux/x86/Makefile
