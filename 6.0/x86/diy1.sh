74a8134f2a6dd31ab0c8a11ba740034f9afb2b77
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-09-03 13:53:26 +0800

    Update diy1.sh

--- 6.0/x86/diy1.sh
+++ 6.0/x86/diy1.sh
@@ -19,9 +19,9 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/neobird
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
-# beifen
+# beife
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
