0a000c2946be63169ad0a985db95f9221a266592
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-09-04 23:05:59 +0800

    Update diy1.sh

--- 5.15/x86/diy1.sh
+++ 5.15/x86/diy1.sh
@@ -26,6 +26,6 @@
 
 
 #sed -i '$a src-git coverup https://github.com/coverUP2/openwrt-packages.git' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
