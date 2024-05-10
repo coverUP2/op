abb018235e508845ddd43f9645428a077fbe3e5f
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2024-05-11 03:18:09 +0800

    Update diy1.sh

--- 6.0/x86/diy1.sh
+++ 6.0/x86/diy1.sh
@@ -22,7 +22,7 @@
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # beife
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 #sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
