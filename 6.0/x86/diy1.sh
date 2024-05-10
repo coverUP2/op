b1feb660f2749da5a804c8cab4f90cc56b9c113c
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2024-05-11 03:18:19 +0800

    Update diy1.sh

--- 6.0/x86/diy1.sh
+++ 6.0/x86/diy1.sh
@@ -25,4 +25,4 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-#sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
