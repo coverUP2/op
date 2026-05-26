c6416c26e40330ddcb90fd28e05a64e8a56969d1
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2026-05-26 20:27:57 +0800

    Add files via upload

---
+++ 237/diy1.sh
@@ -0,0 +1,30 @@
+# echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+# 上网时间控制
+# git clone https://github.com/sirpdboy/luci-app-timecontrol package/luci-app-timecontrol
+
+# 一键分区
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+
+
+# 任务设置taskplan
+# git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
+
+# luci-app-wizard
+# git clone https://github.com/sirpdboy/luci-app-netwizard.git package/luci-app-netwizard
+
+# 看门狗
+# git clone https://github.com/sirpdboy/luci-app-watchdog package/watchdog
+
+# git clone https://github.com/papagaye744/luci-theme-design package/luci-theme-design
+
+
+#echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+#echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+
+#store商店
+git clone https://github.com/linkease/istore.git package/istore
+
+# lucky大吉
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+
+
---
+++ 237/diy2.sh
@@ -0,0 +1,2 @@
+sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/N60-Pro/g' package/base-files/files/bin/config_generate 
